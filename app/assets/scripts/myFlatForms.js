ff46cfa29e59933b1531eb554e3172c1e1ba32b4
Bassochette <julien.prugne@gmail.com>
2014-07-23 16:15:13 +0200

    wysiwig basique mais fonctionnel: skin à faire mais conserve les indentations retur à la ligne et autre

--- app/assets/scripts/myFlatForms.js
+++ app/assets/scripts/myFlatForms.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
-	//$('textarea').autosize();
+	//Inialisation tinymce pour les textarea wysiwyg
+	tinymce.init({
+		selector: "textarea.wysiwig",
+		plugins: ["autoresize", "lists", "table"],
 
+		css: "/styles/tinymce.css",
+		toolbar: false,
+		menubar: false,
+		statusbar: false
+	});
 });
 
 
